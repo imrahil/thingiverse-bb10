0f0c82e4153050f5b99174caa6b36376a26e009b
Jarek Szczepanski <imrahil@imrahil.com>
2013-08-31 10:45:09 +0200

    Data model: Thing and Creator

---
+++ app/js/model/Creator.js
@@ -0,0 +1,9 @@
+function Creator(id, name, full_name, url, public_url, thumbnail)
+{
+    this.id            = id;
+    this.name          = name;
+    this.full_name     = full_name;
+    this.url           = url;
+    this.public_url    = public_url;
+    this.thumbnail     = thumbnail;
+}
